cabea4ccb07db4e5fb38f3be0ae891a0da4afa30
Odair <odavita32@gmail.com>
2025-02-26 15:25:15 -0300

    iniciando o projeto

--- app/src/main/java/com/odair/exemplo01/MainActivity.kt
+++ app/src/main/java/com/odair/exemplo01/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.odair.exemplo01
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.odair.exemplo01.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var cor = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar!!.hide()
+
+        binding.cor1.setOnClickListener() {
+            cor = "#FF03DAC5"
+            binding.btnTrocarCorFundo.setOnClickListener{
+                binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+            }
         }
+        binding.cor2.setOnClickListener(){
+            cor = "#FFBB86FC"
+            binding.btnTrocarCorFundo.setOnClickListener{
+                binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+            }
+        }
+        binding.cor3.setOnClickListener(){
+            cor = "#03A9F4"
+            binding.btnTrocarCorFundo.setOnClickListener{
+                binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+            }
+        }
+        binding.cor4.setOnClickListener(){
+            cor = "#FF0000"
+            binding.btnTrocarCorFundo.setOnClickListener{
+                binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+            }
+        }
+
     }
 }
